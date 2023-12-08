4526771bbc98c77709afbd1fd85bf936563539ba
supers3xykwon <125112635+supers3xykwon@users.noreply.github.com>
2023-12-08 19:28:13 +0900

    Update Do yo wanna build a snow man?.py

--- Do yo wanna build a snow man?.py
+++ Do yo wanna build a snow man?.py
@@ -2,25 +2,25 @@ import tkinter as tk
 import turtle
 import random
 
-# tkinter 윈도우 생성
+# generate a tkinter window
 root = tk.Tk()
 root.geometry("600x600")
 root.configure(bg='black')
 
-# turtle 캔버스 생성
+# generate a turtle canvase
 canvas = tk.Canvas(root, width=600, height=600, bg='black')
 canvas.pack()
 
-# turtle 스크린 생성
+# generate a turtle screen
 screen = turtle.TurtleScreen(canvas)
 screen.bgcolor("black")
 
-# 배경색 설정
+# Backgound Color
 turtle.bgcolor('black')
 
-# 눈사람 그리기 함수
+# Define a function that builds a snowman 
 def draw_snowman():
-    # 몸통 그리기
+    # Body
     body = turtle.Turtle()
     body.penup()
     body.goto(0, -150)
@@ -30,7 +30,7 @@ def draw_snowman():
     body.circle(100)
     body.end_fill()
 
-    # 머리 그리기
+    # Head
     head = turtle.Turtle()
     head.penup()
     head.goto(0, 30)
@@ -40,7 +40,7 @@ def draw_snowman():
     head.circle(70)
     head.end_fill()
 
-    # 눈 그리기
+    # Eyes
     left_eye = turtle.Turtle()
     left_eye.penup()
     left_eye.goto(-30, 100)
@@ -59,7 +59,7 @@ def draw_snowman():
     right_eye.circle(10)
     right_eye.end_fill()
 
-    # 당근 그리기
+    # Carrot
     carrot = turtle.Turtle()
     carrot.penup()
     carrot.goto(0, 75)
@@ -71,7 +71,7 @@ def draw_snowman():
     carrot.goto(0, 75)
     carrot.end_fill()
 
-    # 입 그리기
+    # Mouth
     smile = turtle.Turtle()
     smile.penup()
     smile.goto(-5, 45)
@@ -80,7 +80,7 @@ def draw_snowman():
     smile.color("black")
     smile.circle(60, 45)
 
-    # 팔 그리기
+    # Arms
     right_arm = turtle.Turtle()
     right_arm.penup()
     right_arm.goto(75, -10)
@@ -97,7 +97,7 @@ def draw_snowman():
     left_arm.pencolor("#a52a2a") # 갈색
     left_arm.goto(-110, -10)
 
-    # 바닥 그리기
+    # Gound
     Ground = turtle.Turtle()
     Ground.penup()
     Ground.goto( 300,-140)
@@ -111,7 +111,7 @@ def draw_snowman():
     Ground.goto( 300, -140)
     Ground.end_fill()
 
-# 눈 그리기 함수
+# Define a function that makes snow
 def draw_snow():
     snow = turtle.Turtle()
     snow.hideturtle()
@@ -125,13 +125,13 @@ def draw_snow():
     snow.circle(3)
     snow.end_fill()
 
-# 윈도우 창 설정
+# Setting a Window
 turtle.setup(width=600, height=600)
 
-# 눈사람 그리기
+# Draw Snowman command
 draw_snowman()
 
-# 눈 내리는 효과 추가
+# Draw Snow command
 cnt = 1
 while True:
     draw_snow()
@@ -140,7 +140,7 @@ while True:
     if cnt > 15 :
         break
 
-# "Have a happy Christmas" 텍스트 추가
+# Add some comments
 text = turtle.Turtle()
 text.penup()
 text.goto(-200, -200)
@@ -148,5 +148,5 @@ text.color('orange')
 text.write("Happy Merry Holly Jolly Christmas", font=("Arial", 20, "bold"))
 text.goto(-200, -230)
 text.write("And Happy New Year ♥", font=("Arial", 20, "bold"))
-# 그리기 완료 후 종료하기
+# end.
 turtle.done()
